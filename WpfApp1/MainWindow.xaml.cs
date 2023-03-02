598d324ae3b06aa9dddf68731ad38a7ead85b998
HP <HP@HP>
2023-03-03 00:43:00 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Random r = new Random();
+            SolidColorBrush brush = new SolidColorBrush(Color.FromArgb((byte)r.Next(1, 255), (byte)r.Next(1, 255), (byte)r.Next(1, 255), (byte)r.Next(1, 255)));
+            string messageBoxText = "";
+            if (sender is Button a)
+            {
+                a.Background = brush;
+                messageBoxText = $"Button{a.Content}={a.Background}";
+                ;
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Information;
+                MessageBox.Show(messageBoxText, "Information", button, icon);
+            }
+
+        }
+        private void Button_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Button b)
+            {
+                gr.Children.Remove(b);
+                Title = b.Content.ToString();
+            }
+        }
+    }
+}
